b3074e508e5a2c1906d99fad04543026ab932cde
Sufyan <syedsufyan132649@gmail.com>
2025-05-08 20:32:00 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+// const num =prompt("Enter Your Name");
+// console.log('num');
+
+// alert('hello' + num);
+
+
+// let num1= +prompt("Enter Number");
+
+
+// if (num1 % 2 === 0){
+//     console.log("Even")
+// }
+// else {
+//     console.log("Odd")
+// }
+
+let age = 19;
+console.log("Age = " , age , "Age" );
+
+let inmonths =age*12;
+console.log("Months = " , inmonths );
+
+let indays =inmonths*365;
+console.log("Days = " , indays );
+
+let inhours =indays*24;
+console.log("Hours = " , inhours);
+
+let inminutes =inhours*60;
+console.log("Minutes = " +inminutes);
+
+let inseconds =inminutes*60;
+console.log("Seconds = " +inseconds );
